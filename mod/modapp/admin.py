46ebe0fda4b72263e2694c6ac7c198b93de93275
Dmitriy <ampilog1@gmail.com>
2022-12-14 12:54:35 +0300

    создан файл для заполнения базы

--- mod/modapp/admin.py
+++ mod/modapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-from modapp.models
+from .models import Region, Vacancy, Skills, NumberOffer, FullOffer
 # Register your models here.
+
+admin.site.register(Region)
+admin.site.register(Vacancy)
+admin.site.register(Skills)
+admin.site.register(NumberOffer)
+admin.site.register(FullOffer)
