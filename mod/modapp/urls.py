097dfcfbb9c6694417f56ac1274b3a58d342b7a5
Dmitriy <ampilog1@gmail.com>
2022-12-24 14:42:26 +0300

    код из лекции

--- mod/modapp/urls.py
+++ mod/modapp/urls.py
@@ -1,6 +1,7 @@
 from modapp import views
 from django.urls import path
 
+app_name = 'modapp'
 
 urlpatterns = [
     path('', views.main_view),
--- mod/modapp/views.py
+++ mod/modapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from .models import FullOffer
 
 
 # Create your views here.
 def main_view(request):
-    pass
-    render(request, 'modapp/index.html', context={})
+    fulls = FullOffer.objects.all()
+    return render(request, 'modapp/index.html', context={'fulls': fulls})
